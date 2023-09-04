300a7465c870c0dd1362480588ae8b4d6312b088
gradan-hash <mutukunyaa@gmail.com>
2023-09-04 16:00:56 +0300

    i18n

---
+++ src/I18n.js
@@ -0,0 +1,19 @@
+import i18next from "i18next";
+import Backend from "i18next-http-backend"; //public/locales/{lang}/translation.json
+import LanguageDetector from "i18next-browser-languagedetector";
+import { initReactI18next } from "react-i18next";
+
+i18next
+  .use(Backend)
+  .use(Cache)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: "en",
+    debug: false,
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18next;
